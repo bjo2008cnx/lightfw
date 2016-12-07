8c47d41ff7c535654f7fc2a7f3f21c6494c7d233
bjo2008cn <18600882246@163.com>
2016-12-07 21:18:17 +0800

    add listener. can RUN !

--- src/main/java/org/lightfw/tutorial/web/JavassitContextListener.java
+++ src/main/java/org/lightfw/tutorial/web/JavassitContextListener.java
@@ -1,8 +1,9 @@
 package org.lightfw.tutorial.web;
 
 
+import javassist.*;
 import lombok.extern.log4j.Log4j2;
-import org.lightfw.utilx.dynamic.JavassitUtil;
+import org.lightfw.tutorial.helper.Javassitee;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -14,14 +15,28 @@ import javax.servlet.ServletContextListener;
 public class JavassitContextListener implements ServletContextListener {
 
     public void contextInitialized(ServletContextEvent contextEvent) {
-        System.out.println("容器初始化................");
+        System.out.println("Listener start................");
         String className = "org.lightfw.tutorial.helper.Javassitee";
         System.out.println(className);
-        JavassitUtil.replaceMethodBody(className, "execute", "System.out.println(\"Javassitee's execute() is changed dynamically!\");");
-        System.out.println("容器初始化完成");
+        replaceMethodBody(className, "execute", "System.out.println(\"Javassitee's execute() is changed dynamically!\");");
+        new Javassitee().execute(); //调用被Mock的类
+        System.out.println("Listener started");
+    }
+
+    public static void replaceMethodBody(String className, String methodName, String newMethodBody) {
+        try {
+            ClassPool classPool = ClassPool.getDefault();
+            classPool.insertClassPath(new ClassClassPath(JavassitContextListener.class));
+            CtClass e =classPool.get(className);
+            CtMethod method = e.getDeclaredMethod(methodName);
+            method.setBody(newMethodBody);
+            e.toClass();
+        } catch (CannotCompileException | NotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void contextDestroyed(ServletContextEvent sc) {
-        System.out.println("容器安全关闭");
+        System.out.println("Listener stopped");
     }
 }
