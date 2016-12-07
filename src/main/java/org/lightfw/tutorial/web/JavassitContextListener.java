70aacd592a0dda0d8bf6c3142b01b3d47cb9ea38
bjo2008cn <18600882246@163.com>
2016-12-07 21:33:08 +0800

    add listener. can RUN !

--- src/main/java/org/lightfw/tutorial/web/JavassitContextListener.java
+++ src/main/java/org/lightfw/tutorial/web/JavassitContextListener.java
@@ -1,9 +1,9 @@
 package org.lightfw.tutorial.web;
 
 
-import javassist.*;
 import lombok.extern.log4j.Log4j2;
 import org.lightfw.tutorial.helper.Javassitee;
+import org.lightfw.utilx.dynamic.JavassitUtil;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -18,22 +18,9 @@ public class JavassitContextListener implements ServletContextListener {
         System.out.println("Listener start................");
         String className = "org.lightfw.tutorial.helper.Javassitee";
         System.out.println(className);
-        replaceMethodBody(className, "execute", "System.out.println(\"Javassitee's execute() is changed dynamically!\");");
+        JavassitUtil.replaceMethodBody(className, "execute", "System.out.println(\"Javassitee's execute() is changed dynamically!\");");
         new Javassitee().execute(); //调用被Mock的类
         System.out.println("Listener started");
-    }
-
-    public static void replaceMethodBody(String className, String methodName, String newMethodBody) {
-        try {
-            ClassPool classPool = ClassPool.getDefault();
-            classPool.insertClassPath(new ClassClassPath(JavassitContextListener.class));
-            CtClass e =classPool.get(className);
-            CtMethod method = e.getDeclaredMethod(methodName);
-            method.setBody(newMethodBody);
-            e.toClass();
-        } catch (CannotCompileException | NotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void contextDestroyed(ServletContextEvent sc) {
